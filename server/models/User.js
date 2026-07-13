c1168da718db3fb7dc647ade2970aa1ad109ea9b
akashzone <akashnadar.dev@gmail.com>
2026-07-13 20:39:03 +0530

    feat: create User schema with validations

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      length: 6,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["student", "businessOwner", "admin"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+
+const User = mongoose.model("User",userSchema);
+module.exports = User;
